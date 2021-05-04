a0b4995149dbf5451970991944546b10ece42f2b
ayehappy8 <67134235+ayehappy8@users.noreply.github.com>
2021-05-03 22:28:02 -0400

    creacion

--- paqueteV/mainV.java
+++ paqueteV/mainV.java
@@ -3,9 +3,40 @@ import java.util.Scanner;
 import java.io.*;
 public class  mainV{
 public static void main(String[] args) {
+    creadorDeArchivo("juanismoDocumento");
+    eleminadorDeArchivo("juanismoDocumento");
     Scanner sc = new Scanner(System.in);
-    File archivo = new File("paqueteV/datos/datosMain.txt");
-    System.out.print("se puede leer : " + archivo.canRead());
+    String nombreCliente;
+    System.out.println("escriba su nombre");
+    nombreCliente = sc.next();
+    try {
+    File archivo = new File("paqueteV/datos/datosClinete.txt");
+        }catch (Exception e) {
+            System.out.println("Error");
+        }
     }
-    
+public static void creadorDeArchivo(String nombre){
+    try {
+        File archivo = new File("paqueteV/datos/"+ nombre +".txt");
+        if(archivo.createNewFile()){
+            System.out.println("Archivo Creado: " + archivo.getName());
+        } else {
+            System.out.println("El archivo descrito ya existe");
+        }
+            }catch (Exception e) {
+                System.out.println("Hubo un error en la creacion");
+            }
+}
+public static void eleminadorDeArchivo(String nombre){
+    try {
+        File archivo = new File("paqueteV/datos/"+ nombre +".txt");
+        if(archivo.delete()){
+            System.out.println("Archivo eleminado " + archivo.getName());
+        } else {
+            System.out.println("El archivo no existe");
+        }
+            }catch (Exception e) {
+                System.out.println("Hubo un error en la creacion");
+            }
+}
 }
--- paqueteV/mascotas.java
+++ paqueteV/mascotas.java
@@ -1,5 +1,5 @@
 package paqueteV;
 
 public class mascotas {
-    
+
 }
